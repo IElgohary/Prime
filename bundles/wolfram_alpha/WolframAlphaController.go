71f17764e428f8a8fb7e3bec450cb26319650e55
Islam Elgohary <islam.o.elgohary@gmail.com>
2017-11-01 22:51:02 +0200

    modified structure

---
+++ bundles/wolfram_alpha/WolframAlphaController.go
@@ -0,0 +1,32 @@
+package wolfram_alpha
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"Prime/bundles/common"
+)
+
+type WolframAlphaController struct {
+	common.Controller
+}
+
+func (c *WolframAlphaController) HandleQuery(message string) (*Response, error) {
+
+	// instance of response
+	response := new(Response)
+	// send http request to wolfram alpha api
+	url := os.Getenv("BASE_URL") + "&input=" + message + "&appid=" + os.Getenv("APP_ID") + "&includepodid=Result"
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	// Decode response to json
+	json.NewDecoder(resp.Body).Decode(&response)
+	return response, nil
+	// // return json response
+	// json.NewEncoder(w).Encode(response)
+
+}
